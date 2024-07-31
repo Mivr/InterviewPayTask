3a768ddd84cf3c08df9c37ecc2024e6886f0ac39
Mihail Vratchanski <mivrkiki@gmail.com>
2024-07-31 10:00:42 +0300

    Templates generation

---
+++ src/main/java/com/example/emerchantpaytask/EmerchantPayTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.emerchantpaytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmerchantPayTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmerchantPayTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emerchantpaytask/EmerchantPayTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emerchantpaytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmerchantPayTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
